f2622e400c7bf4dffd7edc379ee61e2be7263906
Almuhandith <almuhandith1497@gmail.com>
2023-10-14 17:04:04 +0100

    card fix

--- Home/components/HowItWorksSection/styles.ts
+++ Home/components/HowItWorksSection/styles.ts
@@ -2,10 +2,12 @@ import genStyles from '../CustomStyles';
 
 const styles = {
   sectionContainer: `${genStyles.pagePad}`,
-  number: `rounded-full p-2 px-2 text-white bg-purple-500 flex justify-center items-center`,
+  number: `rounded-full p-2 px-2 text-white bg-purple-500 flex justify-center items-center max-h-[40px]`,
   center: 'flex justify-center items-center',
-  grid: 'grid grid-cols-1 md:grid-cols-2 mx-auto gap-4',
-  card: 'flex gap-4 p-4 border rounded-lg shadow-lg bg-white'
+  grid: 'grid grid-cols-1 md:grid-cols-3 mx-auto gap-4',
+  card: 'flex gap-4 p-4 border rounded-lg shadow-lg bg-white',
+  cardContainer: 'border-[1px] shadow-lg bg-white py-8 px-4 flex-1 min-h-[200px] rounded-md flex gap-4',
+  header: 'md:text-5xl text-3xl font-semibold md:leading-[4rem] leading-[3rem] mb-4 text-center',
 }
 
 export default styles
