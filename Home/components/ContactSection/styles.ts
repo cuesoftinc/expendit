82cf26862ad92e205c42b94d9ef526a726f36138
Almuhandith <almuhandith1497@gmail.com>
2023-10-15 13:06:06 +0100

    Home page

--- Home/components/ContactSection/styles.ts
+++ Home/components/ContactSection/styles.ts
@@ -4,7 +4,8 @@ const styles = {
   sectionContainer: `${genStyles.pagePad}`,
   innerContainer: 'bg-secondary text-white rounded-3xl min-h-screen p-6',
   header: `mb-10 ${genStyles.header}`,
-  btn: `mt-10 p-2 ${genStyles.btn}`
+  btn: `mt-10 p-2 ${genStyles.btn}`,
+  grid: 'grid grid-cols-1 sm:grid-cols-2 m-auto p-8'
 }
 
 export default styles;
