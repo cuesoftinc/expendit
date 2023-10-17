ad24ff24dacc60bab0dcde76974903ac364bac79
Femtech-web <bossfemzy10@gmail.com>
2023-10-17 13:01:47 +0100

    made some changes to the section of my homepage

--- Home/components/CTASection/styles.ts
+++ Home/components/CTASection/styles.ts
@@ -3,10 +3,10 @@ import genStyles from '../CustomStyles';
 const styles = {
   sectionContainer: `${genStyles.pagePad} min-h-screen`,
   sectionHeader: `${genStyles.header} text-center`,
-  subtext: 'text-center md:text-lg text-base',
+  subtext: `${genStyles.subtext} text-center md:text-lg`,
   featuresContainer: 'flex md:flex-row flex-col mt-[7%] gap-12',
   header: `${genStyles.header}`,
-  subtextTwo: 'md:text-lg text-base mb-4',
+  subtextTwo: `${genStyles.subtext} md:text-lg mb-4`,
   features: 'flex flex-col gap-4',
   feature: 'flex items-center gap-3 font-semibold md:text-base text-sm',
   btn: `${genStyles.btn} px-12 py-4`,
