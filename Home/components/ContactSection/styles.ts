75dc1e77ac6a3cbc481da6df69101fecc7a8f11d
Almuhandith <almuhandith1497@gmail.com>
2023-10-19 00:15:14 +0100

    login page

--- Home/components/ContactSection/styles.ts
+++ Home/components/ContactSection/styles.ts
@@ -5,7 +5,15 @@ const styles = {
   innerContainer: 'bg-secondary text-white rounded-3xl min-h-screen p-6',
   header: `mb-10 ${genStyles.header}`,
   btn: `mt-10 p-2 ${genStyles.btn}`,
-  grid: 'grid grid-cols-1 sm:grid-cols-2 m-auto p-8'
+  grid: 'grid grid-cols-1 sm:grid-cols-2 m-auto p-8',
+  listItem: "mb-16",
+  itemWrapper: "flex gap-4",
+  icon: "text-purple-500 text-2xl",
+  itemParagraph: "md:w-96 mt-4",
+  form: "bg-purple-200 p-8 md:mx-12 rounded-lg",
+  inputContainer: "flex flex-col text-slate-800 py-2",
+  input: "rounded-lg mt-2 p-2 focus:border-slate-300 focus:bg-slate-100",
+  submit: "cursor-pointer w-full mx-auto"
 }
 
 export default styles;
--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -7,11 +7,12 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    theme: {
+      extend: {
+        colors: {
+          secondary: "#121212",
+          input: "#EDEEF9"
+        }
       },
     },
   },
