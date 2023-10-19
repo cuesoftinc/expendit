b9ff5ebca36993165a2161b197430cb1eb7a5627
Almuhandith <almuhandith1497@gmail.com>
2023-10-19 17:45:13 +0100

    update

--- Home/components/ContactSection/styles.ts
+++ Home/components/ContactSection/styles.ts
@@ -13,9 +13,8 @@ const styles = {
   form: "bg-purple-200 p-8 md:mx-12 rounded-lg",
   inputContainer: "flex flex-col text-slate-800 py-2",
   input: "rounded-lg mt-2 p-2 focus:border-slate-300 focus:bg-slate-100",
-  submit: "cursor-pointer w-full mx-auto"
+  submit: "cursor-pointer w-full mx-auto",
   btn: `mt-10 p-2 py-4 text-center rounded-md text-white bg-secondary hover:opacity-90`,
-  grid: 'grid grid-cols-1 sm:grid-cols-2 m-auto p-8'
 }
 
 export default styles;
