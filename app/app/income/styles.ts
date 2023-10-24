1ad9b7729a020b9f7bc32784fd64c1de5c7d3bbe
Almuhandith <almuhandith1497@gmail.com>
2023-10-24 17:34:52 +0100

    categories complete

--- app/app/income/styles.ts
+++
@@ -1,9 +0,0 @@
-const styles = {
-  form: "p-6 md:mx-12 rounded-lg",
-  inputContainer: "flex flex-col text-slate-800 py-2",
-  input: "rounded-lg my-2 mb-2 p-2 focus:border-slate-300",
-  submit: "cursor-pointer w-full mx-auto",
-  btn: `mt-10 p-2 py-2 text-center rounded-md text-white bg-secondary hover:opacity-90`,
-}
-
-export default styles
