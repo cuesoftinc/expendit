923443e28e070ff44d55c48b2b0c50e69a08c2ce
Almuhandith <almuhandith1497@gmail.com>
2023-10-24 15:23:24 +0100

    expense category page complete

--- app/app/income/page.tsx
+++ app/app/income/page.tsx
@@ -1,10 +1,56 @@
+"use client"
+
 import PageLayout from '@/components/layouts/PageLayout'
-import React from 'react'
+import styles from './styles'
+import React, { useState, useEffect, useMemo } from 'react';
 
 const Income = () => {
+  const [items, setItems] = useState<string[]>([]);
+  const [selectedItemIndex, setSelectedItemIndex] = useState<number | null>(null);
+
+  useEffect(() => {
+    const categories = JSON.parse(localStorage.getItem('items') || '[]');
+    setItems(categories);
+  }, []);
+
+  const memoizedItems = useMemo(() => items, [items]);
   return (
     <PageLayout>
-      <h1>Income</h1>
+      <div>
+        <h1 className='mt-8 flex justify-center font-bold text-2xl'>Add a Budget</h1>
+        <form className={styles.form}>
+          <div className={styles.inputContainer}>
+            <label htmlFor='name'>Amount </label>
+            <input className={styles.input} type="text"/>
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor='email'>Date </label>
+            <input className={styles.input} type="date"/>
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor='subject'>Category </label>
+            <select
+              className={styles.input}
+              onChange={(e) => setSelectedItemIndex(parseInt(e.target.value, 10))}
+              value={selectedItemIndex !== null ? selectedItemIndex.toString() : ""}
+            >
+              <option value="">Choose a category</option>
+              {memoizedItems.map((item, index) => (
+                <option key={index} value={index.toString()}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className={styles.inputContainer}>
+            <label>Note </label>
+            <textarea className={styles.input}/>
+          </div>
+          <div className={styles.btn}>
+            <input className={styles.submit} type="submit" value="Submit"/>
+          </div>
+        </form>
+      </div>
     </PageLayout>
   )
 }
