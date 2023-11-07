1792a8800a79ecdf07bad3c331ee75cdfee8ac18
Almuhandith <almuhandith1497@gmail.com>
2023-11-07 13:46:07 +0100

    landing page restructuring

--- Home/app/page.tsx
+++ Home/app/page.tsx
@@ -5,11 +5,13 @@ import HeroSection from '@/components/HeroSection/Index';
 import FeaturesSection from '@/components/FeaturesSection/Index';
 import CTASection from '@/components/CTASection/Index';
 import Contact from "@/components/ContactSection";
+import Services from "@/components/ServiceSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Services />
       <FeaturesSection />
       <CTASection />
       <HowItWorks />
--- Home/components/HowItWorksSection/styles.ts
+++ Home/components/HowItWorksSection/styles.ts
@@ -4,12 +4,11 @@ const styles = {
   sectionContainer: `${genStyles.pagePad}`,
   center: 'flex justify-center items-center',
   grid: 'grid grid-cols-1 md:grid-cols-3 mx-auto gap-4',
-  card: 'flex gap-4 p-4 border rounded-lg shadow-lg bg-white',
-  cardContainer: 'border-[1px] shadow-lg bg-white py-8 px-4 flex-1 min-h-[200px] rounded-md',
-  cardTitle: 'text-xl flex justify-center',
-  cardContent: 'text-center mt-4',
+  cardContainer: 'py-8 px-4 flex-1 min-h-[200px]',
+  cardTitle: 'text-xl',
+  cardContent: 'mt-4',
   header: 'md:text-5xl text-3xl font-semibold md:leading-[4rem] leading-[3rem] mb-10 text-center',
-  icon: 'flex justify-center text-purple-500 text-2xl mb-4',
+  icon: 'text-purple-600 bg-purple-300 rounded-md text-2xl mb-4 w-10 h-10 p-2',
 }
 
 export default styles
