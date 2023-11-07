6dad2e4871e5fa87e6cbf617d957652cc5136fd8
Femtech-web <bossfemzy10@gmail.com>
2023-11-07 05:15:13 +0100

    Refactored some sections in the homepage design

--- Home/components/HeroSection/styles.ts
+++ Home/components/HeroSection/styles.ts
@@ -1,19 +1,19 @@
 import genStyles from '../CustomStyles';
 
 const styles = {
-  heroContainer: `${genStyles.pagePad} min-h-screen`,
+  heroContainer: `${genStyles.pagePad} min-h-screen `,
   navContainer: `${genStyles.flexBtw} py-4`,
   navLinksContainer: 'flex gap-[3.5rem] text-xl font-semibold',
   btnOne: `${genStyles.btn} px-8 py-3`,
   btnTwo: `${genStyles.btn} px-14 py-4`,
   heroSection: 'md:mt-[8%] mt-[15%] flex md:flex-row flex-col gap-10',
   header: `${genStyles.header}`,
-  subtext: `${genStyles.subtext} md:text-lg mb-6`,
+  subtext: `${genStyles.subtext} mb-6 sm:w-[80%] w-[93%] md:text-base text-sm`,
   imgContainer: `${genStyles.flexCenter} md:w-[45%] w-full`,
-  img: 'object-contain w-[60%]',
+  img: 'object-contain w-[60%] md:-translate-y-[3.5rem] -translate-y-0',
 
   // Mobile Navbar Styles 
-  mobileNavCont: 'w-[98%] min-h-[500px] absolute top-0 left-[1%] purple-glassmorphism text-white p-4',
+  mobileNavCont: 'w-[98%] min-h-[500px] absolute top-0 left-[1%] glassmorphism text-white p-4',
   closeNavCont: 'w-full flex justify-end py-6 px-4',
   mobileNavlinks: 'flex flex-col gap-6 min-h-[300px]',
 }
--- app/components/expense/styles.ts
+++ app/components/expense/styles.ts
@@ -1,9 +1,9 @@
 import genStyles from '../CustomStyles';
 
 const styles = {
-  header: 'text-center md:text-xl text-base font-semibold',
-  select: 'w-full mb-4 outline-none px-4 py-4 bg-white/50 text-sm rounded-lg focus:border-2 shadow-sm border-black/80 border-[1px]',
-  textarea: 'w-full p-4 bg-white/50 text-sm rounded-lg focus:border-2 shadow-sm border-black/80 border-[1px] resize-none',
+  header: 'text-center md:text-left md:text-xl text-base font-semibold mb-6',
+  select: 'w-full mb-4 outline-none px-4 bg-grayTheme py-4 text-sm rounded-lg focus:border-2 shadow-sm border-black/80 border-[1px]',
+  textarea: 'w-full p-4 text-sm bg-grayTheme rounded-lg focus:border-2 shadow-sm border-black/80 border-[1px] resize-none',
 };
 
 export default styles;
