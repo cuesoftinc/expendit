4e1d8b4b339113c1c3a9c502a1e1439ca0b3a638
Aniki Yusuf <anikiyusuf99@gmail.com>
2023-11-13 19:38:34 +0100

    models updated, routes added, middleware,controller, readme file , main file update

--- CommonServer/configs/env.go
+++ CommonServer/configs/env.go
@@ -1,13 +1,13 @@
-package config
+ package config
 
-import (
-	"github.com/joho/godotenv"
-	"log"
-)
+// import (
+// 	"github.com/joho/godotenv"
+// 	"log"
+// )
 
-func LoadEnv() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
+// func LoadEnv() {
+// 	if err := godotenv.Load(); err != nil {
+// 		log.Println("No .env file found")
+// 	}
 	
-}
+// }
