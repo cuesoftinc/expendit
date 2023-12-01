8e6b722f980ef8cd87430adc6c70093f58a6e2d8
Femtech-web <bossfemzy10@gmail.com>
2023-12-01 16:27:51 +0100

    fixed the protected routes component and made some changes

--- App/src/components/signup/states.ts
+++ App/src/components/signup/states.ts
@@ -108,6 +108,9 @@ export const useSignUpCustomState = () => {
     formSuccess,
     formLoading,
     handleChange,
-    handleSubmit
+    handleSubmit,
+    setFormLoading,
+    setFormError,
+    setFormSuccess,
   }
 }
--- App/src/context/sessionProvider.tsx
+++ App/src/context/sessionProvider.tsx
@@ -1,26 +1,62 @@
 "use client"
 
-import React, { createContext, useEffect, ReactNode, useState } from 'react';
+import { getLocalStorageItem } from '@/utils/localStorage';
+import React, { createContext, useEffect, useContext, ReactNode, useState, Dispatch, SetStateAction } from 'react';
+import { useRouter } from 'next/navigation';
+import { jwtDecode } from 'jwt-decode';
+import { logoutApi } from '@/API/APIS/authApi';
 
 export interface SessionProviderProps {
   children: ReactNode;
 }
 
-export const SessionContext = createContext<string | null>(null);
+export interface SessionContextProps {
+  sessionToken: string | null;
+  setSessionToken: Dispatch<SetStateAction<string | null>>;
+  isLoading: boolean;
+  setIsLoading: Dispatch<SetStateAction<boolean>>;
+}
+
+export const SessionContext = createContext<SessionContextProps | undefined>(undefined);
 
 export const SessionProvider: React.FC<SessionProviderProps> = ({ children }) => {
   const [sessionToken, setSessionToken] = useState<string | null>(null);
+  const [ isLoading, setIsLoading ] = useState<boolean>(false);
+  const router = useRouter();
 
   useEffect(() => {
-    const storedToken = localStorage.getItem('token');
-    setSessionToken(storedToken);
-  }, []);
+    const storedValue: string | null = getLocalStorageItem("Expendit-token");
+    const token: string | null = storedValue !== null ? JSON.parse(storedValue) : null;
+
+    if(token && token !== null){
+      const decodedToken = jwtDecode(token);
+      
+      if(decodedToken.exp !== undefined){
+
+        if(decodedToken.exp * 1000 < new Date().getTime()) 
+        (async () => await logoutApi({router, setIsLoading}))();
+      }
+    } else {
+      router.push('/signin');
+    }
+  }, [router]);
+
+  // useEffect(() => {
+  //   const storedToken = localStorage.getItem('token');
+  //   setSessionToken(storedToken);
+  // }, []);
 
   return (
-    <SessionContext.Provider value={sessionToken}>
+    <SessionContext.Provider value={{sessionToken, setSessionToken, setIsLoading, isLoading}}>
       {children}
     </SessionContext.Provider>
   );
 };
 
-export const useSession = () =>  React.useContext(SessionContext);
+export const useSession = (): SessionContextProps => {
+  const context = useContext(SessionContext);
+  if (!context) {
+    throw new Error("useNavContext must be used within a AppProvider");
+  }
+  return context;
+};
