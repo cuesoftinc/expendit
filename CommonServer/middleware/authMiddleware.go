4c86b183a178e66bd9390c11a07598236ee85ced
Femtech-web <bossfemzy10@gmail.com>
2023-12-12 01:33:19 +0100

    created the CORS middleware and fixed the create-income api route

--- CommonServer/middleware/authMiddleware.go
+++ CommonServer/middleware/authMiddleware.go
@@ -4,44 +4,50 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	
 	helper "expendit-server/helpers"
 	"github.com/gin-gonic/gin"
 )
 
 
-func Authenticate() gin.HandlerFunc{
-       return func(c *gin.Context){
-		// if c.Request.Method == "OPTIONS" {
-		// 	// Handle preflight request
-		// 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		// 	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		// 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Token") 
-		// 	c.Writer.WriteHeader(http.StatusNoContent)
-		// 	c.Abort()
-		// 	return
-		// }
-
-		clientToken := c.Request.Header.Get("Token")
-		if clientToken == ""{
-			c.JSON(http.StatusInternalServerError, gin.H{"error":fmt.Sprintf("No Authorization header provided")})
-		    c.Abort()
-			return
-		}
-
-		claims, err := helper.ValidateToken(clientToken)
-		if err != "" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "No Authorization header provided"})
 			c.Abort()
 			return
 		}
 
+		// Extract token from the "Bearer" token format
+		tokenParts := strings.Split(authHeader, " ")
+		if len(tokenParts) != 2 || strings.ToLower(tokenParts[0]) != "bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Authorization header format"})
+			c.Abort()
+			return
+		}
+
+		clientToken := tokenParts[1]
+		
+		claims, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		fmt.Println(claims)
+
+		// Set user information in Gin context
 		c.Set("email", claims.Email)
 		c.Set("first_name", claims.First_name)
 		c.Set("last_name", claims.Last_name)
 		c.Set("uid", claims.Uid)
 		c.Set("user_type", claims.User_type)
+
 		c.Next()
 	}
 }
+
 
