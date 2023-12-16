cb1fd976f48dcfc66b834d63bc92bb2e58b23773
Aniki Yusuf <anikiyusuf99@gmail.com>
2023-12-16 14:08:04 +0100

    feat:category function

---
+++ CommonServer/routes/categoryRouter.go
@@ -0,0 +1,18 @@
+package routes
+
+
+
+import (
+	"expendit-server/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func CategoryRoutes(incomingRoutes *gin.Engine){
+	
+	incomingRoutes.GET("/category", controller.GetCategories())
+	incomingRoutes.GET("/category/:id", controller.GetCategoryById())
+	incomingRoutes.POST("/category/create",controller.CreateCategory())
+	incomingRoutes.PUT("/category/:id",controller.UpdateCategory())
+	incomingRoutes.DELETE("/category/:id",controller.DeleteCategory())
+	incomingRoutes.GET("/category/search", controller.SearchCategory())
+}
