6befdaf57648877944a2c1491899c872e63f7842
Femtech-web <bossfemzy10@gmail.com>
2024-01-03 15:35:49 +0100

    implemeted all report charts api

--- App/src/API/APIS/reportApi.ts
+++ App/src/API/APIS/reportApi.ts
@@ -3,13 +3,51 @@ import { getLocalStorageItem } from '@/utils/localStorage';
 
 const userID = getLocalStorageItem('Expendit-userID') || null;
 const user_id = userID ? JSON.parse(userID) : null;
+// Overview Area Chart
+export const getAreaHomeChartApi = async () => {
+  try {
+    const { data } = await API.get(`/report/chart/category/${user_id}`);
 
+    if (data) {
+      return data;
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Report Bar Chart
 export const getBarChartApi = async () => {
   try {
     const { data } = await API.get(`/report/monthly/${user_id}`);
 
     if (data) {
-      console.log(data);
+      return data;
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Report Bar Chart
+export const getPieChartApi = async () => {
+  try {
+    const { data } = await API.get(`/report/chart/category/expenses/${user_id}`);
+
+    if (data) {
+      return data;
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Report Line Chart
+export const getLineChartApi = async () => {
+  try {
+    const { data } = await API.get(`/expense/month-expense/${user_id}`);
+
+    if (data) {
       return data;
     }
   } catch (error) {
