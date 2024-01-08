319384187112cdac43038fc871220f21f9ab773e
Aniki Yusuf <anikiyusuf99@gmail.com>
2024-01-08 12:42:01 +0100

    fix:user mail

--- CommonServer/utils/mail.go
+++ CommonServer/utils/mail.go
@@ -12,6 +12,8 @@ var  EMAIL_FROM  string = os.Getenv("EMAIL_FROM")
 var  SMTP_Host  string = os.Getenv("SMTP_Host")
 var  SMTP_User  string = os.Getenv("SMTP_User")
 var  SMTP_Password  string = os.Getenv("SMTP_Password")
+var  SMTP_Port  int = os.Getenv("SMTP_Password")
+
 
 func SendResetPasswordEmail(toEmail, resetToken string) error {
 	m := gomail.NewMessage()
@@ -20,7 +22,7 @@ func SendResetPasswordEmail(toEmail, resetToken string) error {
 	m.SetHeader("Subject", "Reset Password")
 	m.SetBody("text/html", "Click the following link to reset your password: <a href=\"http://localhost:9000/users/reset-password/"+resetToken+"\">Reset Password</a>")
 
-	d := gomail.NewDialer(SMTP_Host, 587, SMTP_User, SMTP_Password)
+	d := gomail.NewDialer(SMTP_Host, SMTP_Port , SMTP_User, SMTP_Password)
 
 	return d.DialAndSend(m)
 }
