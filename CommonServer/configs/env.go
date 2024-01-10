84b9d4b06d0d17b2d0aa25e5acd109830e309a3b
Raji Abdulsamad <104133531+Almuhandith@users.noreply.github.com>
2024-01-10 12:20:26 +0100

    Update env.go
    Secrets removed
    
    Signed-off-by: Raji Abdulsamad <104133531+Almuhandith@users.noreply.github.com>

--- CommonServer/configs/env.go
+++ CommonServer/configs/env.go
@@ -1,11 +1,5 @@
  package config
 
- SMTP_Host = sandbox.smtp.mailtrap.io;
- SMTP_User = 0fa886b84b2a3a;
- SMTP_Password = bd7f22f37af9fc;
- EMAIL_FROM = gmail.expendit.io;
- PORT_NUMBER = 587;
-
 // import (
 // 	"github.com/joho/godotenv"
 // 	"log"
