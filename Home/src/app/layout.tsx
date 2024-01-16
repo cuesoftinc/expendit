b75172977292bd3d4cc1067a64aeb8f6b1806b75
Femtech-web <bossfemzy10@gmail.com>
2024-01-16 20:59:19 +0100

    made a fix in the Home folder

--- Home/src/app/layout.tsx
+++ Home/src/app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode}) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=AR+One+Sans:wght@400;500;600;700&family=Barlow:wght@200;300;400;500;600;700;800;900&family=Cabin:wght@400;500;600;700&family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
