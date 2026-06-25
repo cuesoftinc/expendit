cf7f950c4021f0b534ceb50d5124ef57981c8e2c
Siyanbola Khalid <siyanbolagiggs@gmail.com>
2026-06-25 20:10:36 +0100

    fix: replace hardcoded frontend URL in password reset email with env var

--- api/common/utils/mail.go
+++ api/common/utils/mail.go
@@ -12,13 +12,14 @@ var EMAIL_FROM string = os.Getenv("EMAIL_FROM")
 var SMTP_Host string = os.Getenv("SMTP_Host")
 var SMTP_User string = os.Getenv("SMTP_User")
 var SMTP_Password string = os.Getenv("SMTP_Password")
+var FRONTEND_URL string = os.Getenv("FRONTEND_URL")
 
 func SendResetPasswordEmail(toEmail, resetToken string) error {
 	m := gomail.NewMessage()
 	m.SetHeader("From", EMAIL_FROM)
 	m.SetHeader("To", toEmail)
 	m.SetHeader("Subject", "Reset Password")
-	m.SetBody("text/html", "Click the following link to reset your password: <a href=\"http://localhost:3000/forgotpassword/new_password/?resetToken="+resetToken+"\">Reset Password</a>")
+	m.SetBody("text/html", "Click the following link to reset your password: <a href=\""+FRONTEND_URL+"/forgotpassword/new_password/?resetToken="+resetToken+"\">Reset Password</a>")
 
 	d := gomail.NewDialer(SMTP_Host, 2525, SMTP_User, SMTP_Password)
 
